1e362652d6ba3eaf083787b1a43c04399b47d415
fanchangyong <fanchangyong@gmail.com>
2019-12-02 18:05:13 +0800

    Remove autoprefixer browsers config

--- client/config/loaders.js
+++ client/config/loaders.js
@@ -33,12 +33,6 @@ exports.postcssLoader = {
     plugins: () => [
       require('postcss-flexbugs-fixes'),
       autoprefixer({
-        browsers: [
-          '>1%',
-          'last 4 versions',
-          'Firefox ESR',
-          'not ie < 9',
-        ],
         flexbox: 'no-2009',
       }),
     ],
