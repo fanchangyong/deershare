59c1d2907543a22e064bda51cc48cb57537ba827
fanchangyong <fanchangyong@gmail.com>
2020-01-11 23:28:31 +0800

    Remove unused code

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -6,7 +6,6 @@ import {
   Redirect,
   Route,
 } from 'react-router-dom';
-import produce from 'immer';
 import PropTypes from 'prop-types';
 import NavBar from '../components/NavBar';
 import SendFilePanel from '../components/SendFilePanel';
@@ -22,31 +21,7 @@ class HomePage extends React.Component {
   constructor() {
     super();
     this.state = {
-      downloadCode: '',
-      showSendFileModal: false,
-      showRecvFileModal: false,
-      fileList: [],
     };
-
-    this.onChangeFile = this.onChangeFile.bind(this);
-
-    this.onChangeDownloadCode = e => this.setState({ downloadCode: e.target.value });
-    this.onClickPrepareDownload = this.onClickPrepareDownload.bind(this);
-    this.onHideRecvFileModal = () => this.props.history.push('/');
-
-    this.onShowSendFileModal = () => this.setState({ showSendFileModal: true });
-    this.onHideSendFileModal = () => this.setState({ showSendFileModal: false, fileList: [] });
-  }
-
-  onChangeFile(e) {
-    this.setState(produce(draft => {
-      draft.fileList.push(e.file);
-    }));
-    this.onShowSendFileModal();
-  }
-
-  onClickPrepareDownload() {
-    this.props.history.push(`/r/${this.state.downloadCode}`);
   }
 
   render() {
