e744098f87175c802215ff31f8241a347b17228b
fanchangyong <fanchangyong@gmail.com>
2020-01-28 18:40:30 +0800

    Add sentry for client

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'webrtc-adapter';
 import { Provider } from 'react-redux';
+import * as Sentry from '@sentry/browser';
 import App from './components/App';
 import configureStore from './store/configureStore';
 import configureWS from './actions/configureWS';
@@ -10,6 +11,11 @@ import configureWS from './actions/configureWS';
 const store = configureStore();
 
 configureWS(store.dispatch);
+
+// Configure Sentry
+if (process.env.NODE_ENV === 'production') {
+  Sentry.init({ dsn: 'https://be8db30a0db043f686f64c4fc91682c8@sentry.io/2034634' });
+}
 
 ReactDOM.render(
   <Provider
