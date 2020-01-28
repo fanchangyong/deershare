34eadcf946d95109444b63e8f5a310962be45946
fanchangyong <fanchangyong@gmail.com>
2020-01-28 20:49:02 +0800

    Fix client sentry

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -6,6 +6,7 @@ import {
   Switch,
   Route,
 } from 'react-router-dom';
+import * as Sentry from '@sentry/browser';
 
 import HomePage from '../pages/HomePage';
 
@@ -13,6 +14,17 @@ import './App.css';
 import './App.cm.styl';
 
 class App extends React.Component {
+  componentDidCatch(error, errorInfo) {
+    if (process.env.NODE_ENV === 'production') {
+      Sentry.withScope(scope => {
+        Object.keys(errorInfo).forEach(key => {
+          scope.setExtra(key, errorInfo[key]);
+        });
+        Sentry.captureException(error);
+      });
+    }
+  }
+
   render() {
     return (
       <Router>
