955c2f17e6c5eee3c9903172cdefe5c633c692b2
fanchangyong <fanchangyong@gmail.com>
2020-02-29 19:46:57 +0800

    Update copyright

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -5,7 +5,7 @@ function Footer() {
   return (
     <div className={styles.base}>
       <span>
-        Copyright© 2019 小鹿快传 | 友情链接：
+        Copyright© 2020 小鹿快传 | 友情链接：
       </span>
       <a target="_blank" rel="noopener noreferrer" href="https://wonderfulcv.com">橙子简历</a>
       <a target="_blank" rel="noopener noreferrer" href="mailto:support@deershare.com"> | 联系我们 </a>
