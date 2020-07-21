90d8c1cf8ef66093ca445682ba23e0e72a4942d3
fanchangyong <fanchangyong@gmail.com>
2020-07-21 16:33:39 +0800

    Add Github link

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -8,6 +8,7 @@ function Footer() {
         © 2020 小鹿快传 | <a target="_blank" rel="noopener noreferrer" href="http://beian.miit.gov.cn/">鲁ICP备18047579号-2</a> | 友情链接：
       </span>
       <a target="_blank" rel="noopener noreferrer" href="https://wonderfulcv.com">橙子简历</a>
+      <a target="_blank" rel="noopener noreferrer" href="https://github.com/fanchangyong/deershare"> | GitHub</a>
       <a target="_blank" rel="noopener noreferrer" href="mailto:support@deershare.com"> | 联系我们 </a>
     </div>
   );
