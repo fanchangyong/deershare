9e7b851102fdaaf1efddeb587c213b77c74fa304
fanchangyong <fanchangyong@gmail.com>
2023-06-27 18:40:16 +0800

    Fix ICP number

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -5,7 +5,7 @@ function Footer() {
   return (
     <div className={styles.base}>
       <span>
-        © 2020 小鹿快传 | <a target="_blank" rel="noopener noreferrer" href="http://beian.miit.gov.cn/">鲁ICP备18047579号-2</a> | 友情链接：
+        © 2020 小鹿快传 | <a target="_blank" rel="noopener noreferrer" href="http://beian.miit.gov.cn/">鲁ICP备18047579号-3</a> | 友情链接：
       </span>
       <a target="_blank" rel="noopener noreferrer" href="https://wonderfulcv.com">橙子简历</a>
       <a target="_blank" rel="noopener noreferrer" href="https://github.com/fanchangyong/deershare"> | GitHub</a>
